84309fb61ba63fe75c9fcefb78c775ca6d5393e5
Eugene <eugenekozhenevskiy@gmail.com>
2024-03-12 22:40:12 +0200

    Solution

---
+++ main.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __eq__(self, other):
+        if isinstance(other, Book):
+            return self.title == other.title and self.author == other.author
+        return False
